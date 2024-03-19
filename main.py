e1687c239ea34d3fc05c3f25e4ce5ce4b4c438bb
BrMaro <70138725+BrMaro@users.noreply.github.com>
2024-03-19 12:59:48 +0300

    incomplete minor fixes attempting to add number change

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import pprint
 import pygame
 import time
-
+import numpy as np
 
 pygame.init()
 clock = pygame.time.Clock()
-WIDTH = 800
-WIN = pygame.display.set_mode((WIDTH, WIDTH),pygame.DOUBLEBUF)
+WIDTH = 900
+WIN = pygame.display.set_mode((WIDTH, WIDTH), pygame.DOUBLEBUF)
 pygame.display.set_caption("Sudoku Solver")
 font = pygame.font.Font('freesansbold.ttf', 32)
 game = 9
 
-
-
-SOLVING_SPEED = 0.005
+BORDER_BLINK_INTERVAL = 1000
+SOLVING_SPEED = 0.000005
 
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
@@ -30,9 +29,10 @@ BLACK = (0, 0, 0)
 GREY = (128, 128, 128)
 WHITE = (255, 255, 255)
 TURQUOISE = (64, 224, 208)
+text_rects = []
 
 
-board = [
+original_board = [
     [3, 9, -1, -1, 5, -1, -1, -1, -1],
     [-1, -1, -1, 2, -1, -1, -1, -1, 5],
     [-1, -1, -1, 7, 1, 9, -1, 8, -1],
@@ -43,8 +43,8 @@ board = [
     [6, 7, -1, 1, -1, 5, -1, 4, -1],
     [1, -1, 9, -1, -1, -1, 2, -1, -1]
 ]
-original_board = board
-
+board = original_board
+immediately_solved_board= original_board.copy()
 
 
 class Node:
@@ -56,12 +56,45 @@ class Node:
         self.color = WHITE
         self.width = width
         self.total_rows = total_rows
+        self.border_visible = False
+        self.blink_timer = 0
+        self.blink_interval = BORDER_BLINK_INTERVAL
+        self.clicked = False
 
     def draw(self, win):
-        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
+        if self.border_visible:
+            pygame.draw.rect(win, RED, (self.x, self.y, self.width, self.width), 2)
+        else:
+            pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
 
-    def get_pos(self):
-        return self.row, self.col
+    def blink_if_clicked(self, current_time):
+        if self.clicked:
+            print("Blinking")
+            # Get the current time
+            time_since_last_blink = current_time - self.blink_timer  # Calculate time since last blink
+            if time_since_last_blink >= self.blink_interval:
+                print("time for next blink")
+                self.border_visible = not self.border_visible
+                print(self.border_visible)
+                self.blink_timer = current_time
+
+    def click(self):
+        print(f"Node {self.row, self.col} clicked")
+        self.clicked = True
+
+    def stop_blinking(self):
+        print("Stop blinking")
+        self.clicked = False
+        self.border_visible = False
+
+
+def get_node_clicked(grid):
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+
+    for row in grid:
+        for node in row:
+            if node.x < mouse_x < node.x + node.width and node.y < mouse_y < node.y + node.width:
+                return node.row, node.col
 
 
 def make_grid(rows, width):
@@ -78,13 +111,13 @@ def make_grid(rows, width):
 def draw_grid(win, rows, width):
     gap = width // rows
     for i in range(rows):
-        pygame.draw.line(win, GREY, (0, i * gap), (width, i * gap))
+        pygame.draw.line(win, GREY, (0, i * gap), (width, i * gap), 1)
     for j in range(rows):
-        pygame.draw.line(win, GREY, (j * gap, 0), (j * gap, width))
-    for i in range(0, rows, 3):
-        pygame.draw.line(win, BLACK, (0, i * gap), (width, i * gap), 3)
-    for j in range(0, rows, 3):
-        pygame.draw.line(win, BLACK, (j * gap, 0), (j * gap, width), 3)
+        pygame.draw.line(win, GREY, (j * gap, 0), (j * gap, width), 1)
+    for i in range(0, rows + 3, 3):
+        pygame.draw.line(win, BLACK, (0, i * gap), (width, i * gap), 5)
+    for j in range(0, rows + 3, 3):
+        pygame.draw.line(win, BLACK, (j * gap, 0), (j * gap, width), 5)
 
 
 def draw(win, grid, rows, width):
@@ -96,7 +129,7 @@ def draw(win, grid, rows, width):
     draw_grid(win, rows, width)
 
 
-def render_numbers_on_board(win, width, rows,board):
+def render_numbers_on_board(win, width, rows, board):
     gap = width // rows
     start_y = gap // 2
 
@@ -107,19 +140,19 @@ def render_numbers_on_board(win, width, rows,board):
                 text = font.render(str(board[row][col]) if board[row][col] != -1 else ' ', True, GREY)
                 textRect = text.get_rect()
                 textRect.center = (start_x, start_y)
+                text_rects.append(textRect)
                 win.blit(text, textRect)
             else:
                 text = font.render(str(board[row][col]) if board[row][col] != -1 else ' ', True, BLACK)
                 textRect = text.get_rect()
                 textRect.center = (start_x, start_y)
+                text_rects.append(textRect)
                 win.blit(text, textRect)
             start_x += gap
         start_y += gap
 
 
-
-
-def render_original_board(win,width,rows):
+def render_original_board(win, width, rows):
     gap = width // rows
     start_y = gap // 2
 
@@ -193,56 +226,69 @@ def solve_sudoku_generator(puzzle):
     puzzle[row][col] = -1
 
 
-
 def main(win, width):
     ROWS = 9
 
     program_running = True
     animation_started = False
-
+    clicked_node = None
     solving_generator = solve_sudoku_generator(board)
 
     while program_running:
         grid = make_grid(ROWS, width)
         draw(win, grid, ROWS, width)
         render_original_board(win, width, ROWS)
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 program_running = False
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and not animation_started:
-                    animation_started = True
-                if event.key == pygame.K_ESCAPE and animation_started:
-                    animation_started = False
-                if event.key == pygame.K_RETURN:
-                    for solution_board in solving_generator:
-                        completed_board = solution_board
-                    animation_started = False
-                    solve_sudoku(original_board)
-                    print(original_board)
-                    render_numbers_on_board(win, width, ROWS,original_board)
-                    # render_numbers_on_board()
-                    print("Simulation completed")
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                print("mousedown")
+                if pygame.mouse.get_pressed()[0] and not animation_started:
+                    for row in grid:
+                        for node in row:
+                            if node.x < event.pos[0] < node.x + node.width and node.y < event.pos[
+                                1] < node.y + node.width:
+                                node.click()
+                                clicked_node = node
 
+            if event.type == pygame.KEYDOWN:
+                print("keydown")
+                if event.unicode.isdigit() and 1 <= int(event.unicode) <= 9:
+                    if clicked_node:
+                        row, col = get_node_clicked(grid)
+                        original_board[col][row] = int(event.unicode)
+                        clicked_node.stop_blinking()
+                        clicked_node = None
+
+                if event.key == pygame.K_SPACE and not animation_started:  # START BUTTON
+                    animation_started = True
+
+                if event.key == pygame.K_ESCAPE and animation_started:  # PAUSE BUTTON
+                    animation_started = False
+
+                if event.key == pygame.K_RETURN:
+                    solve_sudoku(immediately_solved_board)
+                    print(immediately_solved_board)
+                    animation_started = False
+                    render_numbers_on_board(win, width, ROWS, immediately_solved_board)
+
+        current_time = pygame.time.get_ticks()
+        for row in grid:
+            for node in row:
+                node.blink_if_clicked(current_time)
 
         if animation_started:
             try:
                 success = next(solving_generator)
-                render_numbers_on_board(win, width, ROWS,board)
+                render_numbers_on_board(win, width, ROWS, board)
                 time.sleep(SOLVING_SPEED)
 
-                if not success:
-                    print("Solved")
-                    animation_started = False
-
             except StopIteration as e:
-                print("Solved")
+                print("No more steps")
                 animation_started = False
-
+        pygame.display.update()
     pygame.quit()
-
 
 
 main(WIN, WIDTH)
